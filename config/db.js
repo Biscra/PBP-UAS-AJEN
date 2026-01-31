c782892a3658e788fc01d8149e3e1e51f4de7d4a
Ajen M <diajengwulan2000@gmail.com>
2026-01-31 17:07:45 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,10 @@
 const mysql = require('mysql2')
-const dotenv = require('dotenv')
-dotenv.config()
 
-const pool = mysql.createPool({
-    host : process.env.DB_HOST,
-    user : process.env.DB_USER,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_NAME
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '', 
+    database: 'tokoobat'
 })
 
-const poolPromise = pool.promise()
-
-module.exports = poolPromise
+module.exports = db.promise()
