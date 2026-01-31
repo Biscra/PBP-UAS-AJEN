70944de843805f82f7ca7ae002919277ea066cf8
Ajen M <diajengwulan2000@gmail.com>
2026-01-31 17:15:47 +0700

    Refactor addStokin and add delStokin function

--- models/stokinModel.js
+++ models/stokinModel.js
@@ -4,11 +4,18 @@ const getAllStokin = async () =>{
     const [rows] = await db.query("select * from stok_masuk")
     return rows
 }
-const addStokin = async(kode_sm)=>{
-    const {kode_sm, quatity, tanggal_masuk} = stok_masuk
-    const query = "insert into stok_masuk" +"(kode_sm, quatity, tanggal_masuk)" + "values (?,?,?)"
-    const affected = await db.query (query, [kode_sm, quatity, tanggal_masuk])
+const addStokin = async(stok_masuk) =>{
+    const {kode_sm, quantity, tanggal_masuk} = stok_masuk
+    const query = "insert into stok_masuk" + "(kode_sm, quantity, tanggal_masuk)" + "values (?,?,?)"
+    const affected = await db.query (query, [kode_sm, quantity, tanggal_masuk])
     return affected[0].affectedRows
 }
+const delStokin = async(id)=>{
+    const [result] = await db.query(
+        "DELETE FROM stok_masuk WHERE id=?",
+        [id]
+    )
+    return result.affectedRows
+}
 
-module.exports = {getAllStokin,addStokin}
+module.exports = {getAllStokin,addStokin,delStokin}
